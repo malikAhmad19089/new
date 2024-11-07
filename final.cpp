126fb5b285605e589f16a718b016f7af8a503171
malikAhmad19089 <ahmadshahzad3246@gmail.com>
2024-11-07 23:35:08 +0500

    Update final.cpp

--- final.cpp
+++ final.cpp
@@ -1,7 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-//print hello world
-    cout<<"hello world";
+
+int factorial(int n) {
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    return 0;
 }
